8584e1c0bd064b77c31341919bcdbfb6c659dc03
Emmanuel Ikechukwu Akwuba <emmanuelakwuba57@gmail.com>
2025-03-08 22:48:22 +0100

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,11 @@ const router = express.Router();
 import message from "./message";
 import user from "./user";
 import image from "./image";
+import payment from "./payment"
 
 router.use("/messages", message);
 router.use("/users", user);
 router.use("/images", image);
+router.use("/payments",payment);
 
 export default router;
